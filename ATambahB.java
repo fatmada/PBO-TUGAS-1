c838ba83aa74e18e73ea176c3bb7e408ce18549b
fatmada <90265678+fatmada@users.noreply.github.com>
2021-09-14 19:34:30 +0700

    Add files via upload

---
+++ ATambahB.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class ATambahB {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int hasil = a+b;
+        System.out.println(hasil);
+    }
+}
---
+++ BurungBeo.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+import java.util.Scanner;
+public class BurungBeo {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        String kalimat = scan.nextLine();
+        System.out.println(kalimat);
+    }
+}
